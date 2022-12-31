e48a955fb6b826bd366a7f67eff11c32deaf75a2
matheushquintino <matheus.henrique.quintino@gmail.com>
2022-12-30 23:22:27 -0300

    Aula 6

---
+++ funcoes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funcoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Console.ReadLine().ToString();
+            int b = Convert.ToInt32(a);
+            Tabuada(b);
+
+        }
+        public static void Tabuada(int numero)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(numero + " X " + i + " = " + (numero * i));
+            }
+        }
+    }
+}
